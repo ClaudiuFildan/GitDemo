f7f4eb3d4a0a44fbbcd4444995a5103cbc6658dc
rahul <claudiu.fildan@gmail.com>
2020-07-29 18:38:51 +0300

    first commit

---
+++ Section5Subsection27/src/package1/Uiautomaortest.java
@@ -0,0 +1,30 @@
+package package1;
+
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+public class Uiautomaortest extends base {
+	
+	//30.
+	public static void main(String[] args) throws MalformedURLException {
+		
+		AndroidDriver<AndroidElement> driver=Capabilities();
+		
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		//driver.findElementByAndroidUIAutomator("attribute("value")");
+		
+		driver.findElementByAndroidUIAutomator("text(\"Views\")").click();
+		//driver.findElementByAndroidUIAutomator("text(\"Views\")").click();
+		
+		//  Validate clickable feature for all options
+		//  driver.findElementsByAndroidUIAutomator("new UiSelector().property(value)");
+		
+	    System.out.println(driver.findElementsByAndroidUIAutomator("new UiSelector().clickable(value)").size());
+		
+	}
+
+}
